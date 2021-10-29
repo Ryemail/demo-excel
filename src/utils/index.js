d7170baac8631a7ab9d12f91156e8cbb9fae7680
MY <ruany@golive.cc>
2021-10-29 17:21:31 +0800

    excel 导出

---
+++ src/utils/index.js
@@ -0,0 +1,49 @@
+export function columnNameToNumber(name) {
+	if (!name || typeof name !== "string") return;
+
+	name = name.toUpperCase();
+	let sum = 0;
+	for (let i = 0; i < name.length; i++) {
+		sum = sum * 26;
+		sum = sum + (name[i].charCodeAt(0) - "A".charCodeAt(0) + 1);
+	}
+
+	return sum;
+}
+
+export function transformRange(string) {
+	if (!string || typeof string !== "string") return;
+
+	const column = string.replace(/[0-9]/g, "").split(":"),
+		row = string.replace(/[A-Z]/g, "").split(":");
+
+	return {
+		s: { r: +row[0], c: columnNameToNumber(column[0]) },
+		e: { r: +row[1], c: columnNameToNumber(column[1]) },
+	};
+}
+
+export function merge(merges, current, type) {
+	for (let i = 0; i < merges.length; i++) {
+		const val = merges[i];
+
+		if (val.s.r == current.r + 1 && val.s.c === current.c + 1) {
+			const { s, e } = val;
+			const col = e.c - s.c,
+				row = e.r - s.r;
+
+			if (type === "col") return col ? col + 1 : null;
+
+			if (type === "row") return row ? row + 1 : null;
+		}
+	}
+	return null;
+}
+
+export function rowspan(merges, current) {
+	return merge(merges, current, "row");
+}
+
+export function colspan(merges, current) {
+	return merge(merges, current, "col");
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,171 @@
+<template>
+	<div class="home">
+		<button class="button">
+			<input
+				class="upload-input"
+				type="file"
+				@change="onFileChange($event.target)"
+			/>
+			上传文件
+		</button>
+
+		<button @click="exportExcel" class="button">导出文件</button>
+
+		<table>
+			<tr v-for="(row, rowIndex) in data" :key="rowIndex">
+				<template v-for="(cell, cellIndex) in row" :key="cellIndex">
+					<td
+						v-if="cellVisible({ r: rowIndex, c: cellIndex })"
+						:colspan="
+							colspan(merges, { r: rowIndex, c: cellIndex })
+						"
+						:rowspan="
+							rowspan(merges, { r: rowIndex, c: cellIndex })
+						"
+					>
+						{{ cell }}
+					</td>
+				</template>
+			</tr>
+		</table>
+	</div>
+</template>
+
+<script>
+import XlsxPopulate from "xlsx-populate";
+import { saveAs } from "file-saver";
+import { reactive } from "@vue/reactivity";
+import { transformRange, colspan, rowspan } from "@/utils";
+
+import "./style.less";
+
+export default {
+	name: "Home",
+	setup() {
+		const data = reactive([
+			["标题", "", ""],
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9],
+		]);
+
+		const merges = reactive([
+			// transformRange("A1:B2"),
+			transformRange("A1:C1"),
+			transformRange("C2:C4"),
+		]);
+
+		const onFileChange = (file) => {
+			XlsxPopulate.fromDataAsync(file.files[0]).then((workbook) => {
+				const sheet = workbook.sheet(0);
+
+				const result = [];
+
+				sheet._rows.forEach((row) => {
+					const cellValue = [];
+
+					row._cells.forEach((cell) => {
+						cellValue.push(cell.value());
+					});
+
+					result.push(cellValue);
+				});
+
+				const range = sheet.usedRange(); // 获取合并的单元格
+				range._endCell.columnName(); // 获取列名
+
+				return workbook.outputAsync({ type: "" });
+			});
+		};
+
+		const exportExcel = () => {
+			XlsxPopulate.fromBlankAsync()
+				.then((workbook) => {
+					const sheet = workbook.sheet(0);
+
+					sheet
+						.cell("A1")
+						.value(data)
+						.style("border", true)
+						.style("fontSize", 18); // 设置样式
+
+					sheet.cell("A1").style("fill", {
+						type: "pattern",
+						pattern: "darkDown",
+						foreground: "ff0000",
+						background: {
+							theme: 3,
+							tint: 0.4,
+						},
+					}); // 设置样式
+
+					const range = sheet.range("A1:C1"); //获取区间
+
+					// range.merged(true); // 合并单元格
+
+					// 获取区间的单元格
+					range.cells().map((cell) => {
+						return cell.map((val) => val.value());
+					});
+
+					sheet.cell("B1").style("fill", "ff33ff");
+					sheet.cell("B2").style("fill", "9933ff");
+
+					sheet.column("B").width(20); // 设置列宽
+
+					data.forEach((val, index) => {
+						sheet.row(index + 1).height(30); // 设置行高
+					});
+
+					// sheet
+					//     .cell("A1")
+					//     .value("bold")
+					//     .style("bold", true)
+					//     .relativeCell(1, 0)
+					//     .value("italic")
+					//     .style("italic", true)
+					//     .relativeCell(1, 0)
+					//     .value("underline")
+					//     .style("underline", true);
+
+					// return workbook.toFileAsync("./out.xlsx");
+					return workbook.outputAsync({ type: "" });
+				})
+				.then((blob) => {
+					saveAs(blob, "导出.xlsx");
+				});
+		};
+
+		const cellVisible = (current) => {
+			const c = current.c + 1;
+			const r = current.r + 1;
+
+			let visible = true;
+
+			for (let i = 0; i < merges.length; i++) {
+				let { s, e } = merges[i];
+
+				if (s.c === c && e.c === c && s.r < r && r <= e.r) {
+					visible = false;
+					break;
+				} else if (s.r === r && e.r === r && s.c < c && c <= e.c) {
+					visible = false;
+					break;
+				}
+			}
+
+			return visible;
+		};
+
+		return {
+			onFileChange,
+			exportExcel,
+			data,
+			merges,
+			colspan,
+			rowspan,
+			cellVisible,
+		};
+	},
+};
+</script>
